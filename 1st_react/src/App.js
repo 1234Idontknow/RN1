84c056eedfb565aa7f489559381bb198f889a643
Jacob <somma0714@gmail.com>
2022-11-09 16:53:15 -0700

    add files

---
+++ 1st_react/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        This was made at 1:48
+        </p>
+        <p> go to http://localhost:3000/</p>
+        <a href="http://localhost:3000/"> http://localhost:3000/</a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
