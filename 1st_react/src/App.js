a8b3f094c11aa070eb936eb29761bc780bb6511b
Jacob <somma0714@gmail.com>
2022-12-01 17:05:07 -0700

    added:show

--- 1st_react/src/App.js
+++ 1st_react/src/App.js
@@ -1,27 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react"
 
 function App() {
+   function showO (){
+  setCircle("X")
+} 
+function showX() {
+  setCircle("O")
+}
+  const [circle, setCircle]=useState(5)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        This was made at 1:48
-        </p>
-        <p> go to http://localhost:3000/</p>
-        <a href="http://localhost:3000/"> http://localhost:3000/</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button title = "O" onClick={(circle==="X")? (showX) : showO}>
+          {(circle)}</button>
       </header>
     </div>
   );
-}
-
+} 
 export default App;
